e51695804c075e2e2c7d07f0304b6d8b5d2eadf2
sudagoniabhishek <abhisheksudagoni@gmail.com>
2023-03-20 15:49:46 +0530

    first commit

---
+++ WebContent/customer-list.jsp
@@ -0,0 +1,121 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+    <%@ page import="com.techpalle.entity.Customer" %>
+      <%@ page import="java.util.ArrayList" %>
+            <%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"    %>
+      
+      
+      
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Customer list</title>
+<!-- CDN > Content Delivery Network -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<header style="color:white">
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Reliance Retail  Management</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Customer</a>
+        </li>
+      </ul>
+ 
+    </div>
+  </div>
+</nav>
+
+</header>
+<br/>
+<br/>
+
+<div class="container">
+
+	<h2 align="center">Reliance  Customers List</h2>
+	<hr>
+
+	<form action="insertForm">
+		<button class="btn btn-success">Add Customer</button>
+	</form>
+	
+	<table class="table">
+	  <thead>
+	    <tr>
+	      	<th scope="col">Id</th>
+			<th scope="col">Name</th>
+			<th scope="col">Email</th>
+			<th scope="col">Mobile</th>
+			<th scope="col">Actions</th>
+	    </tr>
+	  </thead>
+	  
+	  <tbody>
+		
+		<c:forEach var="i" items="${al}" >
+			<tr>
+				<td><c:out value="${i.id}"></c:out></td>
+				<td><c:out value="${i.name}"></c:out></td>
+				<td><c:out value="${i.email}"></c:out></td>
+				<td><c:out value="${i.mobile}"></c:out></td>
+				
+									<!-- Query String  -->
+				<td> 
+					<a href="editForm?id=<c:out value="${i.id}"></c:out>">Edit</a> 
+					&nbsp;&nbsp; 
+					<a href="delete?id=<c:out value="${i.id}"></c:out>">Delete</a> 
+				</td>
+			</tr>
+		</c:forEach>
+	  
+	  		
+	  </tbody>
+	  
+	  </table>
+	
+</div>
+
+
+
+
+</body>
+</html>
+
+
+<!-- 
+<table border="1"   align="center">
+<tr>
+                         <th>ID</th>
+                          <th>Name</th>
+                           <th>Email</th>
+                           <th>Mobile</th>
+                           <th>Actions</th>
+                           </tr>
+                           <%
+                           ArrayList<Customer> alCustomer=(ArrayList<Customer>)request.getAttribute("al");
+                           for(Customer i :alCustomer)
+                           {
+                        	   out.println("<tr>");
+                        	   out.println("<td>"+i.getId()+"</td>");
+                        	   out.println("<td>"+i.getName()+"</td>");
+                        	   out.println("<td>"+i.getEmail()+"</td>");
+                        	   out.println("<td>"+i.getMobile()+"</td>");
+                        	   out.println("<td>"+"<a>Edit</a> &nbsp; &nbsp; <a>Delete</a>"+"</td>");
+                                out.println("</tr>");
+                           }
+                     %>                        
+</table>
+ -->
+
+
---
+++ src/com/techpalle/dao/CustomerDao.java
@@ -0,0 +1,286 @@
+package com.techpalle.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import com.techpalle.entity.Customer;
+
+public class CustomerDao {
+	
+	
+	
+	private static final String dburl="jdbc:mysql://localhost:3306/customer_management";
+	private static final String dbUserName="root";
+	private static final String dbPassword="abhi";
+	
+	private static Connection con =null;
+	private static PreparedStatement ps  =null;
+	private static Statement stm  =null;
+	private static ResultSet rs =null;
+	
+	private static final String customersListQuery="select * from customer";
+	private static final String customerInsert="insert into customer(name,email,mobile) values(?,?,?)";
+	private static final String customerEditQuery="select * from customer where id=?";
+	private static final String customerUpdateQuery="update customer set name=?,email=?,mobile=? where id=?";
+	private static final String customerDeleteQuery="delete from customer  where id=?";
+	
+
+	
+	public static void deleteCustomer(int id) {
+		
+	
+		try {
+			con=getConnectionDef();
+			ps=con.prepareStatement(customerDeleteQuery);
+			ps.setInt(1, id);
+			
+			ps.executeUpdate();
+				
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		finally {
+		
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public static void editCustomer(Customer c) {
+		
+
+	try {
+		con=getConnectionDef();
+		ps=	con.prepareStatement(customerUpdateQuery);
+		ps.setString(1, c.getName());
+		ps.setString(2, c.getEmail());
+		ps.setLong(3, c.getMobile());
+		ps.setInt(4, c.getId());
+		
+		ps.executeUpdate();
+		
+		
+	} catch (SQLException e) {
+
+		e.printStackTrace();
+	}
+	finally {
+	
+		if(ps!=null) {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	}
+	
+	
+	
+	
+	
+	
+	public static Customer getOneCustomer(int id) {
+	
+		
+	Customer c=null;
+		try {
+			con=getConnectionDef();
+			ps=con.prepareStatement(customerEditQuery);
+			ps.setInt(1, id);
+			
+			rs=ps.executeQuery();
+			
+			rs.next();
+			int i=rs.getInt("id");
+			String n=rs.getString("name");
+			String e=rs.getString("email");
+			long m=rs.getLong("mobile");
+			
+			 c=new Customer(i,n,e,m);
+			
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+		}
+		return c;
+		
+		
+		
+		
+	}
+	
+	
+	public static void addCustomer(Customer customer) {
+	
+		try {
+			con=getConnectionDef();
+			ps=con.prepareStatement(customerInsert);
+			ps.setString(1, customer.getName());
+			ps.setString(2, customer.getEmail());
+			ps.setLong(3, customer.getMobile());
+			
+			ps.executeUpdate();
+			
+			
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+		finally {
+		
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+	
+	
+	public static Connection getConnectionDef() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection(dburl, dbUserName, dbPassword);
+		} catch (ClassNotFoundException e) {
+		
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return con;
+		
+		
+	}
+	
+	public static ArrayList<Customer>  getAllCustomers() {
+		
+		ArrayList<Customer> al=new ArrayList<Customer>();
+		try {
+			con=getConnectionDef();
+			stm=con.createStatement();
+			
+			rs=stm.executeQuery(customersListQuery);
+	
+			while(rs.next()) {
+				
+				int i=rs.getInt("id");
+				String n=rs.getString("name");
+				String e=rs.getString("email");
+				long m=rs.getLong("mobile");
+				
+				Customer c=new Customer( i, n, e, m);
+				al.add(c);
+				
+			}
+			
+			
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+		finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(stm!=null) {
+				try {
+					stm.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+		}
+		return al;
+		
+	}
+	
+	
+	
+}
